e772fdccc869538de6e5f035ac0ab11ef2089cd2
Choi Seungjae <58299653+enopid@users.noreply.github.com>
2026-07-17 09:45:44 +0900

    [level 4] Title: 쌍둥이 빌딩 숲, Time: 0.08 ms, Memory: 4.82 MB -BaekjoonHub

---
+++ 프로그래머스/4/140105. 쌍둥이 빌딩 숲/쌍둥이 빌딩 숲.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+
+//1
+//3
+//5
+//7
+
+using namespace std;
+
+int solution(int n, int count) {
+    int answer = 0;
+    vector<vector<long long>> vecDP(n, vector<long long>(n+1, 0));
+    vecDP[0][1]=1;
+    for(int i=1; i<n; i++) for(int j=0; j<n; j++) {
+        vecDP[i][j]     += vecDP[i-1][j] * (2*i);
+        vecDP[i][j]     %=1000000007;
+        vecDP[i][j+1]   += vecDP[i-1][j];
+        vecDP[i][j+1]   %=1000000007;
+    }
+    answer= vecDP[n-1][count];
+    return answer;
+}
