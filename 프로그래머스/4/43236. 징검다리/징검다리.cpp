3e11d5c4377442e0c951cb92d32359a1f26c394b
Choi Seungjae <58299653+enopid@users.noreply.github.com>
2026-08-09 11:31:06 +0900

    [level 4] Title: 징검다리, Time: 4.10 ms, Memory: 5.71 MB -BaekjoonHub

---
+++ 프로그래머스/4/43236. 징검다리/징검다리.cpp
@@ -0,0 +1,55 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+// a b / c  d  e
+// a / b  c  d  e
+// 0 2 11 14 17 21 25
+//  2 9  3  3  4  4
+
+
+using namespace std;
+
+int solution(int distance, vector<int> rocks, int n) {
+    int answer = 0;
+    sort(rocks.begin(), rocks.end());
+    int iPrevV = 0;
+    vector<int> vecDists;
+    for(int i=0; i<rocks.size(); i++){
+        vecDists.push_back(rocks[i] - iPrevV);
+        iPrevV = rocks[i];
+    }
+    vecDists.push_back(distance - iPrevV);
+    
+    int lv = *min_element(vecDists.begin(), vecDists.end()) -1;
+    int rv = distance +1;
+    while(lv<rv){
+        int mid = (lv+rv)/2;
+        
+        int iCnt = 0;
+        int iSum = 0;
+        for(auto _value : vecDists){
+            if (iSum+_value >= mid){
+                iSum = 0;
+            }
+            else{
+                iSum+=_value;
+                iCnt++;
+            }
+        }
+        
+        //cout << mid << " " << iCnt << endl;
+        
+        if (iCnt>n){
+            rv = mid;
+        }
+        else{
+            lv = mid+1;
+        }
+    }
+    
+    answer = lv-1;
+    
+    return answer;
+}
